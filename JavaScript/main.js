970e1555d987ff0334f6fc9d7933bf041c99e9c7
Mark Andersen <31654229+MarkiiMedia@users.noreply.github.com>
2018-11-30 13:15:26 +0100

    opret
    opret

---
+++ JavaScript/main.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+// TopNav
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
